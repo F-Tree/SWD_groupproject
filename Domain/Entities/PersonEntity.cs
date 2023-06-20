a6c59b583cdd1268a1ba4e229f2365e3fd62a543
Moteyabai <134784925+Moteyabai@users.noreply.github.com>
2023-06-20 15:01:56 +0700

    Tao bang GroupChat va Person(Not done)

---
+++ Domain/Entities/PersonEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class PersonEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime DeathDate { get; set; }
+        public string BirthPlace { get; set; }
+        public string DeathPlace { get; set; }
+        public bool Status { get; set; }
+        public bool Gender { get; set; }
+        public Guid FatherID { get; set; }
+        public Guid MotherID { get; set; }
+        public Guid SpouseID { get; set; }
+
+    }
+}
