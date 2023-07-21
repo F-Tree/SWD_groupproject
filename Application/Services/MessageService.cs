ffbb54bf0ad17d5e92f373ed0ffa724ca53cfb79
Moteyabai <134784925+Moteyabai@users.noreply.github.com>
2023-07-21 13:00:40 +0700

    Message

---
+++ Application/Services/MessageService.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interface;
+using Application.InterfaceRepository;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class MessageService : IMessageService
+    
+    {
+        private readonly IMessageRepository _messageRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MessageService(IMessageRepository messageRepository, IUnitOfWork unitOfWork)
+        {
+            _messageRepository = messageRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<bool> CreateMessageAsync(Guid groupID, string content)
+        {
+            Message newMess = new Message
+            {
+                Content = content,
+                GroupId = groupID,
+
+            };
+            
+            await _messageRepository.AddAsync(newMess);
+            return await _unitOfWork.SaveChangeAsync() > 0;
+        }
+    }
+}
